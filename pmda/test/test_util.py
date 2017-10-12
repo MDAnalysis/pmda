203591c364f81750b4b4244e0bfa0124e4b0fc30
Max Linke <max_linke@gmx.de>
2017-10-12 11:10:11 -0700

    add copyright to test_util.py

--- pmda/test/test_util.py
+++ pmda/test/test_util.py
@@ -1,3 +1,11 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+# PMDA
+# Copyright (c) 2017 The MDAnalysis Development Team and contributors
+# (see the file AUTHORS for the full list of names)
+#
+# Released under the GNU Public Licence, v2 or any higher version
 import time
 from numpy.testing import assert_almost_equal
 
