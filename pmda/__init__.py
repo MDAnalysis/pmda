2f207ef3c04ec6b243773239fafd137e9d46c303
Oliver Beckstein <orbeckst@gmail.com>
2018-05-09 15:29:30 -0700

    pylint fixes
    - exclude _version.py from linting
    - add simle doc string to __init__.py because pylint wants it

--- pmda/__init__.py
+++ pmda/__init__.py
@@ -7,6 +7,25 @@
 #
 # Released under the GNU Public Licence, v2 or any higher version
 
+"""
+PMDA --- Parallel MDAnalysis
+============================
+
+Ready to use analysis and buildings blocks to write parallel analysis
+algorithms using MDAnalysis with dask.
+
+The full documentation can be found at https://www.mdanalysis.org/pmda/.
+
+Note that by default no modules are imported in the top-level name
+space. Import what you need::
+
+   import pmda.parallel
+   import pmda.custom
+   import pmda.rms
+   import pmda.contacts
+
+"""
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
