ebb5f4df1a31ca9d4a5f7973aa9bd4042784d30e
Oliver Beckstein <orbeckst@gmail.com>
2018-10-25 11:18:36 -0700

    add util.make_balanced_blocks() - needed for #71 - not used yet in parallel - tests added - TODO: Needs to work with step

--- pmda/test/test_util.py
+++ pmda/test/test_util.py
@@ -8,10 +8,13 @@
 # Released under the GNU Public Licence, v2 or any higher version
 from __future__ import absolute_import
 
-import time
-from numpy.testing import assert_almost_equal
+import pytest
 
-from pmda.util import timeit
+import time
+import numpy as np
+from numpy.testing import assert_almost_equal, assert_equal
+
+from pmda.util import timeit, make_balanced_blocks
 
 
 def test_timeit():
@@ -19,3 +22,30 @@ def test_timeit():
         time.sleep(1)
 
     assert_almost_equal(timer.elapsed, 1, decimal=2)
+
+@pytest.mark.parametrize("n_frames,n_blocks,result", [
+    (5, 1, [0, 5]),
+    (5, 2, [0, 3, 5]),
+    (5, 3, [0, 2, 4, 5]),
+    (5, 4, [0, 2, 3, 4 ,5]),
+    (5, 5, [0, 1, 2, 3, 4 ,5]),
+    (10, 2, [0, 5, 10]),
+    (10, 3, [0, 4, 7, 10]),
+    (10, 7, [0, 2, 4, 6, 7, 8, 9, 10]),
+])
+@pytest.mark.parametrize("start", (None, 0, 1, 10))
+def test_make_balanced_blocks(n_frames, n_blocks, start, result):
+    start = start if start is not None else 0
+    result = np.array(result) + start
+
+    idx = make_balanced_blocks(n_frames, n_blocks, start=start)
+    assert_equal(idx, result)
+
+@pytest.mark.parametrize("n_frames,n_blocks,start",
+                         [(0, 5, None), (-1, 5, None), (5, 0, None),
+                          (5, -1, None), (0, 0, None), (-1, -1, None),
+                          (5, 4, -1), (0, 5, -1), (5, 0, -1)])
+def test_make_balanced_blocks_ValueError(n_frames, n_blocks, start):
+    with pytest.raises(ValueError):
+        make_balanced_blocks(n_frames, n_blocks, start=start)
+
--- pmda/util.py
+++ pmda/util.py
@@ -12,9 +12,11 @@ Utility functions --- :mod:`pmda.util`
 
 
 """
-from __future__ import absolute_import
+from __future__ import absolute_import, division
 
 import time
+
+import numpy as np
 
 
 class timeit(object):
@@ -30,3 +32,79 @@ class timeit(object):
         self.elapsed = end_time - self._start_time
         # always propagate exceptions forward
         return False
+
+def make_balanced_blocks(n_frames, n_blocks, start=None, step=None):
+    """Divide `n_frames` into `n_blocks` balanced blocks.
+
+    The blocks are generated in such a way that they contain equal numbers of
+    frames when possible, but there are also no empty blocks (which can happen
+    with a naive distribution of ``ceil(n_frames/n_blocks)`` per block and a
+    remainder block).
+
+    Arguments
+    ---------
+    n_frames : int
+        number of frames in the trajectory (>0)
+    n_blocks : int
+        number of blocks (>0)
+    start : int
+        first index of the trajectory (default is None, which is
+        interpreted as "first frame", i.e., 0)
+
+    Returns
+    -------
+    frame_indices : array
+        Array of shape of length ``n_blocks + 1`` with the starting frame of
+        each block; the last index ``frame_indices[-1]`` corresponds to the
+        last index of the last block + 1 so that one can easily use the array
+        for slicing, as shown in the example below.
+
+    Example
+    -------
+    For a trajectory with 5 frames and 4 blocks we get block sizes ``[2, 1, 1,
+    1]`` (instead of ``[2, 2, 1, 0]``).
+
+    The indices will be ``[0, 2, 3, 4, 5]``.
+
+    The indices can be used to slice a trajectory into blocks::
+
+        idx = assign_blocks(5, 4)
+        for i_block, (start, stop) in enumerate(zip(idx[:-1], idx[1:])):
+           for ts in u.trajectory[start:stop]:
+               # do stuff for block number i_block
+
+
+    Notes
+    -----
+    Explanation of the algorithm: For `M` trajectory frames in the trajectory
+    and `N` blocks (or processes), where `i` with 0 ≤ i N-1 is the block number
+    and `m[i]` is the number of frames for block `i` we get a *balanced
+    distribution* (one that does not contain blocks of size 0) with the
+    algorithm ::
+
+        m[i] = M // N     # initial frames for block i
+        r = M % N         # remaining frames 0 ≤ r < N
+        for i in range(r):
+            m[i] += 1     # distribute the remaining frames over the first r blocks
+
+
+    .. versionadded:: 0.2.0
+
+    """
+    start = start if start is not None else 0
+    step = step if step is not None else 1
+
+    if n_frames <= 0:
+        raise ValueError("n_frames must be > 0")
+    elif n_blocks <= 0:
+        raise ValueError("n_blocks must be > 0")
+    elif start < 0:
+        raise ValueError("start must be >= 0")
+    if step != 1:
+        raise NotImplementedError("Only step=1 or step=None is supported")
+    # TODO: use step
+
+    bsizes = np.ones(n_blocks, dtype=np.int64) * n_frames // n_blocks
+    bsizes += (np.arange(n_blocks, dtype=np.int64) < n_frames % n_blocks)
+    frame_indices = np.cumsum(np.concatenate(([start], bsizes)))  # [start, start, start, ...]
+    return frame_indices
