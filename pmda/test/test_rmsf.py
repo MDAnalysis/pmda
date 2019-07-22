4d10a2b09fd46afe2b5e934a3e7c315ee7db1c98
Nik Awtrey <nawtrey@asu.edu>
2019-07-22 09:59:09 -0700

    Added tests for pmda Travis

--- pmda/test/test_rmsf.py
+++ pmda/test/test_rmsf.py
@@ -1,6 +1,9 @@
 import pytest
 import MDAnalysis as mda
 import numpy as np
+import os
+import pytest
+import pmda
 import pmda.rmsf
 from MDAnalysis.analysis.rms import RMSF
 from numpy.testing import assert_almost_equal
@@ -26,3 +29,20 @@ def test_rmsf_values(u):
     PMDA.run(n_blocks=core_number, n_jobs=core_number)
     MDA = mda.analysis.rms.RMSF(u.atoms).run()
     assert_almost_equal(MDA.rmsf, PMDA.rmsf)
+
+
+@pytest.fixture
+def setup(tmpdir):
+    newdir = tmpdir.mkdir('resources')
+    return newdir.dirname
+
+
+def test_nottmp():
+    filepath = os.path.join(os.path.realpath(pmda.__file__))
+    assert os.path.exists(filepath)
+
+
+def test_tmpdir(setup):
+    os.chdir(setup)
+    filepath = os.path.join(os.path.realpath(pmda.__file__))
+    assert os.path.exists(filepath)
