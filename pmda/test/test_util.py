Changes:  - Added tests for second_order_moments() and reduce()  - Added documentation to second_order_moments()  - Fixed mean calculation issue in second_order_moments()  - Added sumofsquares() to util.py for use in tests

pmda/test/test_util.py
@@ -15,8 +15,9 @@ import pytest
 import time
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_equal
+from functools import reduce
 
-from pmda.util import timeit, make_balanced_slices
+from pmda.util import timeit, make_balanced_slices, second_order_moments, sumofsquares
 
 
 def test_timeit():
@@ -130,3 +131,55 @@ def test_make_balanced_slices_ValueError(n_frames, n_blocks,
     with pytest.raises(ValueError):
         make_balanced_slices(n_frames, n_blocks,
                              start=start, stop=stop, step=step)
+
+
+@pytest.mark.parametrize('n_frames', [3, 4, 10, 19, 101, 331, 1000])
+def test_second_order_moments(n_frames):
+    # generate array of random positions in range [-100, 100] for 100 time steps
+    pos = 200*(np.random.random(size=(n_frames, 1000, 3)) - 0.5)
+    # random splitting point
+    isplit = (np.random.randint(1, n_frames-1))
+    # split into two partitions; should be fixed to split by n_blocks partitions
+    p1, p2 = pos[:isplit], pos[isplit:]
+    # create [t, mu, M] lists
+    S1 = [len(p1), p1.mean(axis=0), sumofsquares(p1)]
+    S2 = [len(p2), p2.mean(axis=0), sumofsquares(p2)]
+    # run lists through second_order_moments
+    result = second_order_moments(S1, S2)
+    # compare result to calculations over entire pos array
+    assert result[0] == len(pos)
+    assert_almost_equal(result[1], pos.mean(axis=0))
+    assert_almost_equal(result[2], sumofsquares(pos))
+
+
+@pytest.mark.parametrize('n_blocks', [2, 3, 4, 5, 10, 100, 500])
+@pytest.mark.parametrize('n_frames', [1000, 10000, 50000, 100000])
+def test_reduce(n_frames, n_blocks):
+    # generate array of random positions in range [-100, 100] for 100 time steps
+    pos = 200*(np.random.random(size=(n_frames, 1000, 3)) - 0.5).astype(np.float64)
+    # generate array of indices to split pos by
+    split = []
+    for i in range(n_blocks):
+        split.append(np.random.randint(1, n_frames-1))
+    # remove reduntant indices and sort
+    split = np.sort(np.unique(split))
+    # generate list of block slices
+    split_pos = []
+    for i in range(len(split)):
+        if i == 0:
+            split_pos.append(pos[0:split[i]])
+        elif not i == 0 and not i == len(split)-1:
+            split_pos.append(pos[split[i-1]:split[i]])
+        else:
+            split_pos.append(pos[split[-2]:split[-1]])
+            split_pos.append(pos[split[-1]:n_frames])
+    # create list of [t, mu, M] lists
+    S = []
+    for i in range(len(split_pos)):
+        S.append([len(split_pos[i]), split_pos[i].mean(axis=0, dtype=np.float64), sumofsquares(split_pos[i])])
+    # combine block results using fold method
+    results = reduce(second_order_moments, S)
+    # compare result to calculations over entire pos array
+    assert results[0] == len(pos)
+    assert_almost_equal(results[1], pos.mean(axis=0, dtype=np.float64))
+    assert_almost_equal(results[2], sumofsquares(pos), decimal=5)

pmda/util.py
@@ -185,13 +185,25 @@ def second_order_moments(S1, S2):
 
     Parameters
     ----------
-    S1 : Array
-
-    S2 : Array
+    S1 : array
+        Contains `[T1, mu1, M1]` where `T1` is an integer (number of elements
+        in the partition, e.g., the number of time frames), `mu1` is an
+        `n x m` array of the means for `n` atoms (and for example, `m=3` for
+        the center of geometry), `M1` is also an `n x m` array of the sum of
+        squares.
+    S2 : array
+        Contains `[T2, mu2, M2]` where `T2` is an integer (number of elements
+        in the partition, e.g., the number of time frames), `mu2` is an
+        `n x m` array of the means for `n` atoms (and for example, `m=3` for
+        the center of geometry), `M2` is also an `n x m` array of the sum of
+        squares.
 
     Returns
     -------
-    S : Array
+    S : [T, mu, M]
+        The returned list contains the total number of elements in the
+        partition `T`, the mean `mu` and the "second moment" `M` (sum of
+        squares) for the combined data.
 
     References
     ----------
@@ -199,9 +211,18 @@ def second_order_moments(S1, S2):
     formulae and a pairwise algorithm for computing sample variances."
     Technical Report STAN-CS-79-773, Stanford University, Department of
     Computer Science, 1979.
+
+    .. [Pebay2008] P. Pebay, "Formulas for robust one-pass parallel
+    computation of co-variances and arbitrary-order statistical moments",
+    Technical Report SAND2008-6212, 2008.
     """
     T = S1[0] + S2[0]
-    mu = (S1[1] + S2[1])/2
+    mu = (S1[0]*S1[1] + S2[0]*S2[1])/T
     M = S1[2] + S2[2] + (S1[0] * S2[0]/T) * (S2[1] - S1[1])**2
     S = [T, mu, M]
     return S
+
+
+def sumofsquares(array):
+    """Calculates the sum of squares"""
+    return np.sum((array - array.mean(axis=0))**2, axis=0)