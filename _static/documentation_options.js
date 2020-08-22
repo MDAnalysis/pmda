505c923f2f6827a6eec07667b761e2f4172ab7b3
TravisCI <TravisCI@mdanalysis.org>
2020-08-22 20:51:59 +0000

    rebuilt html docs from branch master with sphinx at 6f2c77e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0+19.g9b3b566',
+    VERSION: '0.3.0+20.g6f2c77e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
